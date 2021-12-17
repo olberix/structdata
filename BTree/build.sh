fa8808d91e7a59d5c3406ba4658dc61f018026e7
ccencon <jxcc2014@foxmail.com>
2021-12-17 15:59:49 +0800

    B树测试代码&readme说明

--- BTree/build.sh
+++ BTree/build.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
 
-clear
-rm -f a.out
 gcc -std=c11 -W -gdwarf-2 -g3 -DDEBUG -g -rdynamic -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 test.c BTree.c ../DlQueue/DlQueue.c ../DuCirLinkList/DuCirLinkList.c ../SqStack/SqStack.c ../SqList/SqList.c
-# echo copy--------
-# rm -f ../../cc.DATA
-# cp ../../reverse.DATA ../../cc.DATA
-echo run---------
-./a.out
-# echo ===========================
-# stat cc.DATA
-# echo ===========================
-# du -sh cc.DATA
-
-# gcc check.c -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 -o ../../dc
--- BTree/test.c
+++ BTree/test.c
@@ -41,7 +41,6 @@ void foreach(const void* pKey, const void* pValue)
 	const TYVALUE* value = pValue;
 	printf("KEY:a=%d b=%lf\tVALUE:a=%d b=%lf c=%s\n", key->a, key->b, value->a, value->b, value->c);
 }
-extern int cccc;
 
 int main(int argc, char const *argv[])
 {
@@ -50,6 +49,7 @@ int main(int argc, char const *argv[])
 	TYVALUE val;
 	memcpy(val.c, "abcdefghijklmnopqrstuvwxyz\0", 27);
 	// val.c = 'e';
+
 	const int range = 750000;
 	puts("111111-----**********");
 	for (int i = 0; i < range; i += 3){
@@ -119,20 +119,19 @@ int main(int argc, char const *argv[])
 		printf("done-insert--3--%d\n", i);
 	}
 
-	// for(int i = range - 1; i >= 0; i--){
-	// 	key.a = i;
-	// 	key.b = i + 0.5;
-	// 	val.a = 3 * i + 1;
-	// 	val.b = val.a + 0.123;
-	// 	BTree().insert(bt, &key, &val);
-	// }
+	for(int i = range - 1; i >= 0; i--){
+		key.a = i;
+		key.b = i + 0.5;
+		val.a = 3 * i + 1;
+		val.b = val.a + 0.123;
+		BTree().insert(bt, &key, &val);
+	}
 
 
 	// BTree().traverse(bt, foreach);
 	// puts("=======================================================");
 	// puts("=======================================================");
 	// BTree().level_order_traverse(bt, foreach);
-
 
 
 	puts("=======================================================");
@@ -143,7 +142,7 @@ int main(int argc, char const *argv[])
 		if (ret){
 			val = TOCONSTANT(TYVALUE, ret);
 			if (!(val.a == key.a * 3 + 1 && val.b == val.a + 0.123)){
-				puts("error---------");
+				printf("%d--error---------", key.a);
 				break;
 			}
 			if (i % 30000 == 0)
