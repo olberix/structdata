04fb610ca290e13a9cf55a41db51a25cccf30e68
ccencon <jxcc2014@foxmail.com>
2022-01-17 09:52:55 +0800

    编译脚本&增加新编译文件

--- BTree/build.sh
+++ BTree/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gcc -std=c11 -W -gdwarf-2 -g3 -DDEBUG -g -rdynamic -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 test.c BTree.c ../DlQueue/DlQueue.c ../DuCirLinkList/DuCirLinkList.c ../SqStack/SqStack.c ../SqList/SqList.c
+gcc -std=c11 -W -gdwarf-2 -g3 -DDEBUG -g -rdynamic -D_GNU_SOURCE -D_FILE_OFFSET_BITS=64 test.c BTree.c ../DlQueue/DlQueue.c ../DuCirLinkList/DuCirLinkList.c ../SqStack/SqStack.c ../SqList/SqList.c ../common/default_func.c
